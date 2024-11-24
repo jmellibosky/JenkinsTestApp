6f6570216118c9e45ef1b898c76409d53ccaaf51
jmellibosky <joaquinm.utn@gmail.com>
2024-11-24 11:22:38 -0300

    Reestructuración

---
+++ JenkinsConsoleApp.Test/SumTest.cs
@@ -0,0 +1,39 @@
+using JenkinsTestApp.Code;
+
+namespace JenkinsConsoleApp.Test
+{
+    public class SumTests
+    {
+        [Fact]
+        public void SumTwoPositiveNumbers()
+        {
+            int SumResult = Sum.SumTwoNumbers(2, 3);
+
+            Assert.Equal(5, SumResult);
+        }
+
+        [Fact]
+        public void SumTwoNegativeNumbers()
+        {
+            int SumResult = Sum.SumTwoNumbers(-2, -3);
+
+            Assert.Equal(-5, SumResult);
+        }
+
+        [Fact]
+        public void SumPositiveAndNegativeNumbers()
+        {
+            int SumResult = Sum.SumTwoNumbers(2, -3);
+
+            Assert.Equal(-1, SumResult);
+        }
+
+        [Fact]
+        public void SumNegativeAndPositiveNumbers()
+        {
+            int SumResult = Sum.SumTwoNumbers(-2, 3);
+
+            Assert.Equal(1, SumResult);
+        }
+    }
+}
